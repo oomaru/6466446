f146f079c4de144b93f4f5b4e8f5cf429baacbce
Omar Farina <omar91farina@yahoo.com>
2023-08-31 12:07:22 +0200

    Carico il progetto

---
+++ src/app/user.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from "@angular/core";
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class UserService {
+     getUsers() {
+        return [
+            {
+              name: 'Luca',
+              lastname: 'Marino',
+              email: 'luca.marino@gmail.com',
+              fiscalcode: 'MRNLCU91A01F205H',
+              province: 'Milano',
+              phone: '+ 39 347 53 87 987',
+              age: 32
+            },
+            {
+              name: 'Michela',
+              lastname: 'Ferro',
+              email: 'michela.ferro@gmail.com',
+              fiscalcode: 'FRRMHL95A41L219R',
+              province: 'Torino',
+              phone: '+ 39 347 53 87 365',
+              age: 28
+            },
+            {
+              name: 'Marco',
+              lastname: 'Rossi',
+              email: 'marco.rossi@gmail.com',
+              fiscalcode: 'RSSMRC80A01H501W',
+              province: 'Roma',
+              phone: '+ 39 347 53 87 145',
+              age: 43
+            },
+            {
+              name: 'Sara',
+              lastname: 'Leone',
+              email: 'sara.leone@gmail.com',
+              fiscalcode: 'SRALNE85A01L219Y',
+              province: 'Torino',
+              phone: '+ 39 347 53 87 456',
+              age: 38
+            }
+          ]
+    }
+
+}
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-users',
+
+  templateUrl: 'users.component.html',
+  styleUrls: [
+    'users.component.css'
+  ]
+})
+
+export class UsersComponent implements OnInit {
+  title = 'Users'
+  public users: any[] = [];
+
+  constructor(private service: UserService) {
+
+
+  }
+  ngOnInit(): void {
+    this.users = this.service.getUsers();
+  }
+
+}
